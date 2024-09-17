d5b42cf6324890ef25620806fda4ae68d8656fbc
kyleorman <kaorman@uark.edu>
2024-09-17 16:59:00 -0500

    Fixed NerdTree/tmux conflict

--- tmux_keys.sh
+++ tmux_keys.sh
@@ -1,61 +1,75 @@
+
 #!/bin/bash
 
-# Get the current pane's command
-current_command=$(tmux display -p "#{pane_current_command}")
+# ~/.tmux_keys.sh
+# Usage: ~/.tmux_keys.sh <key>
 
-# Define functions for actions
-send_keys() {
-  tmux send-keys "$1"
-}
+LOGFILE=~/tmux_key_binding.log
 
-select_pane() {
-  tmux select-pane "$1"
-}
+{
+  echo "Timestamp: $(date)"
+  echo "Key Pressed: $1"
 
-# Define key actions
-case "$1" in
-  h)
-    if [[ "$current_command" =~ ^(g?view|n?vim?x?)(diff)?$ ]]; then
-      send_keys "C-h"
-    else
-      select_pane "-L"
-    fi
-    ;;
-  j)
-    if [[ "$current_command" =~ ^(g?view|n?vim?x?)(diff)?$ ]]; then
-      send_keys "C-j"
-    elif [[ "$current_command" =~ ^fzf$ ]]; then
-      send_keys "C-j"
-    else
-      select_pane "-D"
-    fi
-    ;;
-  k)
-    if [[ "$current_command" =~ ^(g?view|n?vim?x?)(diff)?$ ]]; then
-      send_keys "C-k"
-    elif [[ "$current_command" =~ ^fzf$ ]]; then
-      send_keys "C-k"
-    else
-      select_pane "-U"
-    fi
-    ;;
-  l)
-    if [[ "$current_command" =~ ^(g?view|n?vim?x?)(diff)?$ ]]; then
-      send_keys "C-l"
-    else
-      select_pane "-R"
-    fi
-    ;;
-  \\)
-    if [[ "$current_command" =~ ^(g?view|n?vim?x?)(diff)?$ ]]; then
-      send_keys "C-\\"
-    else
-      select_pane "-l"
-    fi
-    ;;
-  *)
-    echo "Invalid key action"
-    exit 1
-    ;;
-esac
+  # Get the current pane's command
+  current_command=$(tmux display -p "#{pane_current_command}")
+  echo "Current Command: $current_command"
 
+  case "$1" in
+    h)
+      if [[ "$current_command" =~ ^(g?view|n?vim?x?)(diff)?$ ]]; then
+        echo "Action: send-keys C-h"
+        tmux send-keys "C-h"
+      else
+        echo "Action: select-pane -L"
+        tmux select-pane -L
+      fi
+      ;;
+    j)
+      if [[ "$current_command" =~ ^(g?view|n?vim?x?)(diff)?$ ]]; then
+        echo "Action: send-keys C-j"
+        tmux send-keys "C-j"
+      elif [[ "$current_command" =~ ^fzf$ ]]; then
+        echo "Action: send-keys C-j"
+        tmux send-keys "C-j"
+      else
+        echo "Action: select-pane -D"
+        tmux select-pane -D
+      fi
+      ;;
+    k)
+      if [[ "$current_command" =~ ^(g?view|n?vim?x?)(diff)?$ ]]; then
+        echo "Action: send-keys C-k"
+        tmux send-keys "C-k"
+      elif [[ "$current_command" =~ ^fzf$ ]]; then
+        echo "Action: send-keys C-k"
+        tmux send-keys "C-k"
+      else
+        echo "Action: select-pane -U"
+        tmux select-pane -U
+      fi
+      ;;
+    l)
+      if [[ "$current_command" =~ ^(g?view|n?vim?x?)(diff)?$ ]]; then
+        echo "Action: send-keys C-l"
+        tmux send-keys "C-l"
+      else
+        echo "Action: select-pane -R"
+        tmux select-pane -R
+      fi
+      ;;
+    \\)
+      if [[ "$current_command" =~ ^(g?view|n?vim?x?)(diff)?$ ]]; then
+        echo "Action: send-keys C-\\"
+        tmux send-keys "C-\\"
+      else
+        echo "Action: select-pane -L"
+        tmux select-pane -L
+      fi
+      ;;
+    *)
+      echo "Invalid key action: $1"
+      ;;
+  esac
+
+  echo "----------------------------------------"
+} >> "$LOGFILE" 2>&1
